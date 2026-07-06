9e4a96089fd2b0d78388ce8e6c764920556b50fd
adriandunai11 <dunaiadrian1113@gmail.com>
2026-07-06 12:27:07 +0200

    Use recurring ticket service in module controller

--- Controllers/ItTicketsController.php
+++ Controllers/ItTicketsController.php
@@ -2,6 +2,34 @@
 
 namespace App\Modules\ItTickets\Controllers;
 
+use App\Modules\ItTickets\Services\RecurringTicketService;
+
 class ItTicketsController extends \App\Controllers\It_tickets
 {
+    public function runRecurringTaskNow($id)
+    {
+        $this->permissionCheck('view_recurring_it_tickets');
+        postAllowed();
+
+        $result = (new RecurringTicketService())->generateOne((int) $id);
+
+        return $this->response->setJSON([
+            'status' => $result['status'],
+            'message' => $result['message'],
+        ]);
+    }
+
+    public static function generateRecurringTasks(): bool
+    {
+        return (new RecurringTicketService())->generateDueTasks();
+    }
+
+    public function testRecurringTasks()
+    {
+        $this->permissionCheck('manage_it_tickets');
+
+        (new RecurringTicketService())->generateDueTasks();
+
+        return redirect()->to('it_tickets')->with('sSuccess', 'Ismétlődő feladatok generálása lefutott.');
+    }
 }
