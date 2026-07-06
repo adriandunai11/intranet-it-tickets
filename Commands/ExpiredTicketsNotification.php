23cc6b8eccbbf5b83ce8775580226a74d342ffdc
adriandunai11 <dunaiadrian1113@gmail.com>
2026-07-06 11:26:15 +0200

    Use module namespace in root command

--- Commands/ExpiredTicketsNotification.php
+++ Commands/ExpiredTicketsNotification.php
@@ -1,7 +1,18 @@
 <?php
 
-namespace App\Commands;
+namespace App\Modules\ItTickets\Commands;
 
-class ExpiredTicketsNotification extends \App\Modules\ItTickets\Commands\ExpiredTicketsNotification
+use App\Modules\ItTickets\Services\ExpiredTicketsNotificationService;
+use CodeIgniter\CLI\BaseCommand;
+
+class ExpiredTicketsNotification extends BaseCommand
 {
+    protected $group = 'IT tickets';
+    protected $name = 'it_tickets:expired_tickets';
+    protected $description = 'Send notification email with expired tickets';
+
+    public function run(array $params): void
+    {
+        (new ExpiredTicketsNotificationService())->send();
+    }
 }
